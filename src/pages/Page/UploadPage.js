fc2f9dca7ecb8ec7b90a0edbde8c087aca741e03
davidmaxy1994 <maksymiukdavid@gmail.com>
2024-07-24 20:12:25 +0100

    Implemented STR 2: Create 2nd Page used for adding new viedeos
    *Implemented task: Create the page structure
    
    *Still need to implement the following tasks: - [ ]  Create JS logic - [ ]  Apply design

--- src/pages/Page/UploadPage.js
+++ src/pages/Page/UploadPage.js
@@ -1,5 +1,43 @@
+import React from "react";
+import "../Page/UploadPage.scss";
+
 const UploadPage = () => {
-  return <div></div>;
+  return (
+    <>
+      <section className="upload">
+        <h1 className="upload__title">Upload Video</h1>
+        <div className="upload__form">
+          <div className="upload__form-group">
+            <h3 className="upload__form-label">VIDEO THUMBNAIL</h3>
+            <div className="upload__form-thumbnail"></div>
+          </div>
+          <div className="upload__form-group">
+            <h3 className="upload__form-label">TITLE YOUR VIDEO</h3>
+            <input
+              type="text"
+              className="upload__form-input"
+              placeholder="Add a title to your video"
+            />
+          </div>
+          <div className="upload__form-group">
+            <h3 className="upload__form-label">ADD A VIDEO DESCRIPTION</h3>
+            <textarea
+              className="upload__form-textarea"
+              placeholder="Add a video description to your video"
+            ></textarea>
+          </div>
+          <div className="upload__form-actions">
+            <button className="upload__form-button upload__form-button--publish">
+              Publish
+            </button>
+            <button className="upload__form-button upload__form-button--cancel">
+              Cancel
+            </button>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default UploadPage;
