c76c4a0d2aaad7f60bd84f59da6d7005a9766fd7
davidmaxy1994 <maksymiukdavid@gmail.com>
2024-07-28 12:44:24 +0100

    Implemented STR 3: Implement device responsivness

--- src/components/Component/Uploader.js
+++ src/components/Component/Uploader.js
@@ -3,12 +3,10 @@ import UploadIcon from "../Icons/UploadIcon";
 
 const Uploader = () => {
   return (
-    <div className="header__uploader">
-      <button className="header__uploader__button">
-        <UploadIcon />
-        <h3 className="header__uploader__button--text">UPLOAD</h3>
-      </button>
-    </div>
+    <button className="header__uploader__button">
+      <UploadIcon />
+      <h3 className="header__uploader__button--text">UPLOAD</h3>
+    </button>
   );
 };
 
--- src/pages/Page/UploadPage.js
+++ src/pages/Page/UploadPage.js
@@ -5,6 +5,8 @@ import "react-toastify/dist/ReactToastify.css";
 import "../../styles/partials/customToastStyles.css";
 import "../../styles/partials/customToastStyles.css";
 import "../Page/UploadPage.scss";
+import UploadIcon from "../../components/Icons/UploadIcon";
+import thumbnailImage from "../../assets/images/Upload-video-preview.jpg";
 
 const UploadPage = () => {
   const navigate = useNavigate();
@@ -20,46 +22,65 @@ const UploadPage = () => {
 
   return (
     <>
-      <section className="upload">
-        <h1 className="upload__title">Upload Video</h1>
-        <form className="upload__form" onSubmit={handleSubmit}>
-          <div className="upload__form-group">
-            <h3 className="upload__form-label">VIDEO THUMBNAIL</h3>
-            <div className="upload__form-thumbnail"></div>
-          </div>
-          <div className="upload__form-group">
-            <h3 className="upload__form-label">TITLE YOUR VIDEO</h3>
-            <input
-              type="text"
-              className="upload__form-input"
-              placeholder="Add a title to your video"
-            />
-          </div>
-          <div className="upload__form-group">
-            <h3 className="upload__form-label">ADD A VIDEO DESCRIPTION</h3>
-            <textarea
-              className="upload__form-textarea"
-              placeholder="Add a video description to your video"
-            ></textarea>
-          </div>
-          <div className="upload__form-actions">
-            <button
-              type="submit"
-              className="upload__form-button upload__form-button--publish"
-            >
-              Publish
-            </button>
-            <button
-              type="button"
-              className="upload__form-button upload__form-button--cancel"
-              onClick={() => navigate("/")}
-            >
-              Cancel
-            </button>
-          </div>
-        </form>
-        <ToastContainer />
-      </section>
+      <div className="upload-wrapper">
+        <section className="upload">
+          <h1 className="upload__title">Upload Video</h1>
+          <form className="upload__form" onSubmit={handleSubmit}>
+            <div className="upload__form-container">
+              <div className="upload__form-group upload__form-left">
+                <h3 className="upload__form-label h3-light-gray">
+                  VIDEO THUMBNAIL
+                </h3>
+                <div className="upload__form-thumbnail">
+                  <img
+                    src={thumbnailImage}
+                    alt="Video Thumbnail"
+                    className="upload__form-thumbnail-image"
+                  />
+                </div>
+              </div>
+              <div className="upload__form-right">
+                <div className="upload__form-group">
+                  <h3 className="upload__form-label h3-light-gray">
+                    TITLE YOUR VIDEO
+                  </h3>
+                  <input
+                    type="text"
+                    className="upload__form-input"
+                    placeholder="Add a title to your video"
+                  />
+                </div>
+                <div className="upload__form-group h3-light-gray">
+                  <h3 className="upload__form-label">
+                    ADD A VIDEO DESCRIPTION
+                  </h3>
+                  <textarea
+                    className="upload__form-textarea"
+                    placeholder="Add a video description to your video"
+                  ></textarea>
+                </div>
+              </div>
+            </div>
+            <div className="upload__form-actions">
+              <button
+                type="button"
+                className="upload__form-button upload__form-button--cancel"
+                onClick={() => navigate("/")}
+              >
+                <h3>CANCEL</h3>
+              </button>
+              <button
+                type="submit"
+                className="upload__form-button upload__form-button--publish"
+              >
+                <UploadIcon />
+                <h3>PUBLISH</h3>
+              </button>
+            </div>
+          </form>
+          <ToastContainer />
+        </section>
+      </div>
     </>
   );
 };
